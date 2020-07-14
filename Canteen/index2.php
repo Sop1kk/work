c27f81f6e7b1dc8f82944996eeb31f68b53672e0
Sopik <sopik96@mail.ru>
2020-07-14 09:46:44 +0600

    canteen index2 title changed

--- Canteen/index2.php
+++ Canteen/index2.php
@@ -2,7 +2,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Меню Аптекаплюс</title>
+    <title>Столовая Аптекаплюс</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="style.css" rel="stylesheet" type="text/css">
     <?
